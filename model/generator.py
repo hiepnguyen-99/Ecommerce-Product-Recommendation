f723933078e01c8add3051990c5c79fd705fbfaf
hiepnguyen <hiepnguyenn1709@gmail.com>
2025-06-01 22:17:04 +0700

    fine tune facebook bart-base

---
+++ model/generator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4ffb86cf875d96066a81919d05370ae0c5ff5a436a3a6ef4fa66685c417ec76
+size 1766
---
+++ model/retriever.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2136bfa436783fb90d9417065110c6eee61b7f4a3edc42a63276aea1dcc2af11
+size 831
