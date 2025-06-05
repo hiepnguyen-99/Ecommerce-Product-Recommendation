633ccc31cb64bfcd61b41d25d382543c1efe936c
hiepnguyen <hiepnguyenn1709@gmail.com>
2025-06-05 14:25:03 +0700

    change model and prompt

--- model/generator.py
+++ model/generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85f7a2e64006d715262407c6d695e9043f15a74a7732fcc37bb32865b7e12d61
-size 1547
+oid sha256:f50ef2fa1c1107f0bfcec827b519f1123163de16bb7170c61f599d8694920aa7
+size 1636
--- model/retriever.py
+++ model/retriever.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fcdc27c4fd2b581b8521f99cdd1fadab12742fc4b59e3afb783bb0d2c96c853
-size 831
+oid sha256:8892963e251fb7ac78f2e8391b4452c20974b3f073e9ad6eaae97eb086e9f3f1
+size 790
